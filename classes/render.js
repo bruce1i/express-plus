b1232449e5b96e082d93d06056953a77f562ec40
lixun <bruce.lixun@gmail.com>
2017-02-28 15:42:29 +0800

    render修改

--- classes/render.js
+++ classes/render.js
@@ -10,15 +10,12 @@ class render {
             params = {};
         }
 
-        // 添加全局变量到模板
+        // 添加全局变量到模板参数
         if (config.view_params) {
             for (var key in config.view_params) {
                 params[key] = config.view_params[key];
             }
         }
-
-        // 在这里设置每个模版都可以使用的全局模版变量
-        // params.__global__ = 'http://xxx.xxx.cn';
 
         res.render(view, params);
     }
